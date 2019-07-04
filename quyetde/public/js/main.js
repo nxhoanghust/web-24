dce6c573f8a5861c2d0bc857d6b6bc1ffaa397e1
nxhoang <nxhoang99@gmail.com>
2019-07-04 22:24:14 +0700

    buoi 3

---
+++ quyetde/public/js/main.js
@@ -0,0 +1,7 @@
+//query element object
+document.getElementById();
+document.getElementsByClassName();
+document.querySelector();
+document.querySelectorAll();
+//addeventListener
+elementObect.addEventListener();
